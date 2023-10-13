64f9b68e6de53831832e103654c4e453d46afb34
Simon Boddy <bbsimonbb@gmail.com>
2023-10-13 15:59:28 +0200

    works to a degree
    Click to select

--- apps/arrow-select/src/App.tsx
+++ apps/arrow-select/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import testData from "./TestData"
 import { observer } from "mobx-react-lite"
-import { makeAutoObservable } from 'mobx'
+import { action, makeAutoObservable } from 'mobx'
 
 export interface IOption {
   optionName: string
@@ -28,13 +28,13 @@ const withProps: IOption[] = testData.map(o => {
     selectedValue: undefined
   }
 })
-const state = makeAutoObservable({options:withProps})
+const state = makeAutoObservable({ options: withProps })
 
 const App = observer(() => {
   const [count, setCount] = useState(0)
 
   // using the current value of one useState inside the set of another picks up stale values, so we need to keep all this together
-  const keyPressInComponent = (e: KeyboardEvent) => {
+  const keyPressInComponent = action((e: KeyboardEvent) => {
     console.log(e.keyCode)
     if ([37, 38, 39, 40].includes(e.keyCode))
       e.preventDefault()
@@ -65,19 +65,38 @@ const App = observer(() => {
 
       // UP ARROW
       case 38: {
-        if (ptrs.opt !== -1)
+        if (ptrs.val === -1) {
+          // option not active, load existing choice
+          if (state.options[ptrs.opt].selectedIndex !== undefined)
+            // there is an existing choice, load it.
+            ptrs.val = state.options[ptrs.opt].selectedIndex || 0
+          else
+            ptrs.val = state.options[ptrs.opt].optionValues.length - 1
+        }
+        else {
+          // option already active
           ptrs.val = ptrs.val === -1 ? state.options[ptrs.opt].optionValues.length - 1 : (ptrs.val + state.options[ptrs.opt].optionValues.length - 1) % state.options[ptrs.opt].optionValues.length
+        }
         break
       }
       // DOWN ARROW
       case 40: {
-        if (ptrs.opt !== -1)
+        if (ptrs.val === -1)
+          // option not active, load existing choice
+          if (state.options[ptrs.opt].selectedIndex !== undefined)
+            // there is an existing choice, load it
+            ptrs.val = state.options[ptrs.opt].selectedIndex || 0
+          else
+            ptrs.val = 0
+        else {
+          // option already active, advance in the list
           ptrs.val = (ptrs.val + 1) % state.options[ptrs.opt].optionValues.length
+        }
         break
       }
 
     }
-  }
+  })
 
   useEffect(() => {
     function handleKeyPress(e: KeyboardEvent) {
@@ -100,7 +119,9 @@ const App = observer(() => {
       </button>
       <div>{JSON.stringify(ptrs)}</div>
       {state.options.map((o, index) =>
-        <div className={`an-option ${index === ptrs.opt ? 'active' : ''}`}>
+        <div className={`an-option ${index === ptrs.opt ? 'active' : ''}`}
+          onClick={action(() => { ptrs.opt = index; ptrs.val = state.options[index].selectedIndex || 0; })}
+        >
           <div className="option-text">{o.optionText}</div>
           <div className='selected-title'>
             {o.selectedValue?.title}
@@ -108,7 +129,15 @@ const App = observer(() => {
           {index === ptrs.opt && ptrs.val != -1 ?
             <div className='option-values'
               style={{ top: ((ptrs.val * -32) - 0.5) + 'px' }}>
-              {o.optionValues.map((v, vIndex) => (<div className={`option-value ${ptrs.val === vIndex ? 'curr-option-value' : ''}`}     >{v.title}</div>))}
+              {o.optionValues.map((v, vIndex) => (
+                <div className={`option-value ${ptrs.val === vIndex ? 'curr-option-value' : ''}`}
+                  onClick={action(() => {
+                    ptrs.val = -1
+                    state.options[index].selectedIndex = vIndex
+                    state.options[index].selectedValue = state.options[index].optionValues[vIndex]
+                  })}
+                >{v.title}</div>)
+              )}
             </div>
             : null}
         </div>
