74cdbb20576cd65f35f6defb0bc98e3a99103fd8
Simon Boddy <bbsimonbb@gmail.com>
2023-12-13 23:49:07 +0100

    IStateful generic

--- packages/octopus-state-graph/src/IStateful.ts
+++ packages/octopus-state-graph/src/IStateful.ts
@@ -1,8 +1,8 @@
-export interface IStateful{
-    loadState: (state:any)=>void
-    saveState: ()=>any
+export interface IStateful<SerializedShape>{
+    loadState: (state:SerializedShape)=>void
+    saveState: ()=>SerializedShape
 }
 
-export function isStateful(thing) : thing is IStateful{
+export function isStateful(thing) : thing is IStateful<any>{
     return thing.loadState && thing.saveState
 }
